510eb4384cd48bc7db5604d16f8e116eb3b8579c
chandram13 <mschandr@uci.edu>
2023-11-09 10:34:23 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Marvish Chandra
+
+# Roofing calculator
+
+def roofPitch(houseArea):
+roofPitch = [1/12,2/12,3/12,4/12,5/12,6/12,7/12,8/12,9/12,10/12,11/12,12/12,13/12,14/12,15/12,16/12,17/12,18/12,19/12,20/12,21/12,22/12,23/12,24/12] for i in roofPitch: (i * houseArea) return i
+
+
+def roofAngle(houseArea,roofAngle):
+    myroofangle = houseArea * roofAngle
